423e4c126e2a2703182e5b8f251ef57b39d28d72
Rob Terrell <Rterrell25@gmail.com>
2020-02-21 22:06:29 -0500

    set up login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,34 @@
-import React, { Component } from "react"
-
+import React from "react"
+import { theme } from "../theme"
+import { Link } from "react-router-dom"
+import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <AppBar>
-        <Toolbar>
-          <Button color='inherit'>Home</Button>
-          <Button color='inherit'>Login</Button>
-          <Button color='inherit'>Signup</Button>
-        </Toolbar>
-      </AppBar>
-    )
-  }
+const Navbar = () => {
+  const colors = theme.colors
+  const useStyles = makeStyles(theme => ({
+    root: {
+      backgroundColor: colors.primary
+    }
+  }))
+  const classes = useStyles()
+  return (
+    <AppBar className={classes.root}>
+      <Toolbar className='nav-container'>
+        <Button color='inherit' component={Link} to='/'>
+          Home
+        </Button>
+        <Button color='inherit' component={Link} to='/login'>
+          Login
+        </Button>
+        <Button color='inherit' component={Link} to='/signup'>
+          Signup
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Navbar
